25bd47f6410636856aaa17c7b4110adc2107bd0e
Chris Ludwig <Chris Ludwig>
2016-10-04 14:19:13 -0400

    Added print to index.js

--- js/index.js
+++ js/index.js
@@ -3,3 +3,4 @@
 var githubtest = {};
 githubtest.Version = '0.1';
 githubtest.Info = 'Test file for github repo';
+githubtest.print = function(){console.log('Github test file\nVersion: ' + this.Version + '\nInfo: ' + this.Info)}
